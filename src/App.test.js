586684853b1d9dcb2752ef5960e402c8c5d16b01
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2022-12-22 19:00:20 +0500

    written some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,17 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import mockData from './mockData.json';
 
 describe('renders App Component', () => {
   it('Should render App', async () => {
+
+    // jest.spyOn(global, 'fetch').mockResolvedValue({
+    //   json: jest.fn().mockResolvedValue(mockData)
+    // })
+
     render(<App />);
     await waitFor(() => {
       expect(screen.getByText('bulbasaur')).toBeInTheDocument()
     })
-    expect(linkElement).toBeInTheDocument();
   })
 });
---
+++ src/components/Thumbnail.test.js
@@ -0,0 +1,21 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
+import mockData from './mockData.json';
+
+describe('test thumbnail', () => {
+
+
+    it('Should render App', async () => {
+
+        jest.spyOn(global, 'fetch').mockResolvedValue({
+            json: jest.fn().mockResolvedValue(mockData)
+        })
+
+        render(<App />);
+        await waitFor(() => {
+            expect(screen.getByText('bulbasaur')).toBeInTheDocument()
+        })
+    })
+
+
+})
